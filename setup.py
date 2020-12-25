edf096fd209c95fb2031aa4429cd81a809a413d8
JosiasAurel <ndjosiasaurel@gmail.com>
2020-12-25 13:23:49 +0100

    add package install

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     author="Josias Aurel",
     author_email="ndjosiasaurel@gmail.com",
-    install_requires = ["gql",],
     name='hashnode',
+    install_requires=['gql',],
     version='0.1dev',
     packages=['src',],
     license='MIT',
     url="https://github.com/JosiasAurel/python-hashnode",
+    long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
 )
